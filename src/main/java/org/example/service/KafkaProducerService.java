6be5247b16ad900a33d94c33ea8bbf85314d5b72
kuzmich <tokimecykary1@gmail.com>
2025-09-19 15:56:32 +0300

    refactoring1

--- src/main/java/org/example/service/KafkaProducerService.java
+++ src/main/java/org/example/service/KafkaProducerService.java
@@ -1,19 +1,18 @@
 package org.example.service;
 
+import lombok.RequiredArgsConstructor;
 import org.example.dto.UserEventDTO;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaProducerService {
 
     private final KafkaTemplate<String, UserEventDTO> kafkaTemplate;
 
-    public KafkaProducerService(KafkaTemplate<String, UserEventDTO> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
     public void sendUserEvent(UserEventDTO userEventDTO) {
         kafkaTemplate.send("user-events", userEventDTO);
     }
+
 }
